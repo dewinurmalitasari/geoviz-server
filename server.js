4a9ce901fcd93775878657b84e022f26956ce147
Muhammad Akbar Reishandy <akbar@reishandy.my.id>
2025-10-29 11:23:48 +0700

    feat: add Swagger and Swagger UI for API documentation

--- server.js
+++ server.js
@@ -4,11 +4,53 @@ import auth from "./plugins/auth.js";
 import { config } from 'dotenv'
 import mongoosePlugin from './plugins/mongoose.js'
 import User from "./model/User.js";
+import swagger from '@fastify/swagger'
+import swaggerUi from '@fastify/swagger-ui'
 
 config()
 
 const fastify = Fastify({
     logger: true
+})
+
+// Swagger configuration
+fastify.register(swagger, {
+    swagger: {
+        info: {
+            title: 'GeoViz API Documentation',
+            description: 'Back-End Server for GeoViz',
+            version: '1.0.0'
+        },
+        // host: `${process.env.HOST}:${process.env.PORT}`,
+        schemes: ['http', 'https'],
+        consumes: ['application/json'],
+        produces: ['application/json'],
+        securityDefinitions: {
+            bearerAuth: {
+                type: 'apiKey',
+                name: 'Authorization',
+                in: 'header',
+                description: 'Enter JWT token in the format: Bearer <token>'
+            }
+        }
+    }
+})
+
+// Swagger UI configuration
+fastify.register(swaggerUi, {
+    routePrefix: '/docs',
+    uiConfig: {
+        docExpansion: 'full',
+        deepLinking: false
+    },
+    uiHooks: {
+        onRequest: function (request, reply, next) { next() },
+        preHandler: function (request, reply, next) { next() }
+    },
+    staticCSP: true,
+    transformStaticCSP: (header) => header,
+    transformSpecification: (swaggerObject, request, reply) => { return swaggerObject },
+    transformSpecificationClone: true
 })
 
 fastify.register(mongoosePlugin)
@@ -18,6 +60,7 @@ fastify.register(routes)
 const start = async () => {
     try {
         await fastify.listen({port: process.env.PORT, host: process.env.HOST})
+        console.log(`Documentation available at http://${process.env.HOST}:${process.env.PORT}/docs`)
     } catch (err) {
         fastify.log.error(err)
         process.exit(1)
@@ -29,6 +72,25 @@ start()
 export async function build() {
     const fastify = Fastify({
         logger: false // Disable logger during tests for cleaner output
+    })
+
+    // Register Swagger for test build as well
+    fastify.register(swagger, {
+        swagger: {
+            info: {
+                title: 'GeoViz API Documentation',
+                description: 'Back-End Server for GeoViz',
+                version: '1.0.0'
+            },
+            host: 'localhost:3000', // Default for tests
+            schemes: ['http'],
+            consumes: ['application/json'],
+            produces: ['application/json']
+        }
+    })
+
+    fastify.register(swaggerUi, {
+        routePrefix: '/docs'
     })
 
     fastify.register(mongoosePlugin)
