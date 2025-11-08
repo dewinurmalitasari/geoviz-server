9c835f460a13a6b412abe832633fad52b7718648
Muhammad Akbar Reishandy <akbar@reishandy.my.id>
2025-11-08 13:25:38 +0700

    feat: disable directConnection option in mongoose connection

--- plugins/mongoose.js
+++ plugins/mongoose.js
@@ -4,9 +4,7 @@ import mongoose from 'mongoose'
 export default fp(async function (fastify, opts) {
     return new Promise(async (resolve, reject) => {
         try {
-            await mongoose.connect(process.env.MONGODB_URI, {
-                // directConnection: true,
-            })
+            await mongoose.connect(process.env.MONGODB_URI)
             fastify.log.info('MongoDB connected')
 
             // Expose mongoose to Fastify instance
